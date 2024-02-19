39bd35069264cd6a99f26f8572658832fbc4d252
Chigozie-css <chigoziedivine62@gmail.com>
2024-02-19 04:53:59 -0800

    4 Shell sort - Knuth Sequence

--- sort.h
+++ sort.h
@@ -24,10 +24,10 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
+void selection_sort(int *array, size_t siz);
 void quick_sort(int *array, size_t size);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
+void quicksort_recursive(int *array, int low, int high);
+int lomuto_partition(int *array, int low, int high);
+void shell_sort(int *array, size_t size);
 
 #endif
